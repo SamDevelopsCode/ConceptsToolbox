cb75a87a82645c741dfce190919fc31e4ae9b84c
_booch <fousek.sam@gmail.com>
2024-03-16 23:00:29 -0500

    refactor ConceptLoader class

--- scripts/ConceptInfoLoader.cs
+++ scripts/ConceptInfoLoader.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 
 public partial class ConceptInfoLoader : VBoxContainer
 {
-	[Export] private PackedScene _dotnetInfoScene;
-	[Export] private PackedScene _variablesInfoScene;
-	[Export] private PackedScene _typesInfoScene;
+	[Export] private PackedScene _dotnetContentScene;
+	[Export] private PackedScene _variablesContentScene;
+	[Export] private PackedScene _typesContentScene;
 	
 	[ExportCategory("Main Content Container")]
 	[Export] private VBoxContainer _content;
@@ -26,75 +26,76 @@ public partial class ConceptInfoLoader : VBoxContainer
 	
 	private async void OnButtonPressed(Button button)
 	{
-		// if there is already a info panel showing
 		if (_content.GetChildCount() > 0)
 		{
-			var currentInfoContainer = _content.GetChild(0);
-			if (currentInfoContainer.Name == button.Name)
+			var currentConceptContentInstance = _content.GetChild(0);
+			if (currentConceptContentInstance.Name == button.Name)
 			{
 				return;
 			}
 			
-			await TweenInfoContainerExit(currentInfoContainer);
+			await TweenConceptContentInstanceExit(currentConceptContentInstance);
 		}
 
-		await AddInfoContainer(button);
+		AddConceptContentPage(button.Name);
 	}
 
 	
-	private async Task AddInfoContainer(Button button)
+	private void AddConceptContentPage(string buttonName)
 	{
-		Control infoContainerInstance;
-		switch (button.Name)
+		switch (buttonName)
 		{ 
 			case "DotNet":
-				infoContainerInstance = _dotnetInfoScene.Instantiate<Control>();
-				_content.AddChild(infoContainerInstance);
-				infoContainerInstance.GetNode<Label>("%Title").Text = button.Name;
-				await ToSignal(GetTree(), "process_frame");
-				infoContainerInstance.Scale = new Vector2(.01f, .01f);
-				infoContainerInstance.PivotOffset = infoContainerInstance.Size / 2;
-				TweenInfoContainerEntry(infoContainerInstance);
+				CreateConceptContentInstance(_dotnetContentScene, buttonName);
 				break;
 			case "Variables":
-				infoContainerInstance = _variablesInfoScene.Instantiate<Control>();
-				_content.AddChild(infoContainerInstance);
-				infoContainerInstance.GetNode<Label>("%Title").Text = button.Name;
-				await ToSignal(GetTree(), "process_frame");
-				infoContainerInstance.Scale = new Vector2(.01f, .01f);
-				infoContainerInstance.PivotOffset = infoContainerInstance.Size / 2;
-				TweenInfoContainerEntry(infoContainerInstance);
+				CreateConceptContentInstance(_variablesContentScene, buttonName);
 				break;
 			case "Types":
-				infoContainerInstance = _typesInfoScene.Instantiate<Control>();
-				_content.AddChild(infoContainerInstance);
-				infoContainerInstance.GetNode<Label>("%Title").Text = button.Name;
-				await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
-				infoContainerInstance.Scale = new Vector2(.01f, .01f);
-				infoContainerInstance.PivotOffset = infoContainerInstance.Size / 2;
-				TweenInfoContainerEntry(infoContainerInstance);
+				CreateConceptContentInstance(_typesContentScene, buttonName);
 				break;
 		};
 	}
 
+	
+	private async void CreateConceptContentInstance(PackedScene packedScene, string buttonName)
+	{
+		Control newConceptContentInstance = packedScene.Instantiate<Control>();
+		_content.AddChild(newConceptContentInstance);
+		
+		//using modulation in method instead of hiding/showing because of weird flickering effect
+		newConceptContentInstance.Modulate = new Color(0, 0, 0, 0);
+		// setting the Title of the content page to the name of button node (Why type it when no need?)
+		newConceptContentInstance.GetNode<Label>("%Title").Text = buttonName;
+		
+		// docs suggest to await a process frame before setting scale of a control that is a child of a container
+		await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+		
+		newConceptContentInstance.Modulate = Colors.White;
+		newConceptContentInstance.Scale = new Vector2(.01f, .01f);
+		newConceptContentInstance.PivotOffset = newConceptContentInstance.Size / 2;
+		
+		TweenConceptContentInstanceEntry(newConceptContentInstance);
+	}
 
-	private void TweenInfoContainerEntry(Control infoContainerInstance)
+
+	private void TweenConceptContentInstanceEntry(Control conceptContentInstance)
 	{
 		Tween tween = GetTree().CreateTween();
 		tween.SetParallel();
-		tween.TweenProperty(infoContainerInstance, "scale:x", 1.0f, .3f);
-		tween.TweenProperty(infoContainerInstance, "scale:y", 1.0f, .3f);
+		tween.TweenProperty(conceptContentInstance, "scale:x", 1.0f, .3f);
+		tween.TweenProperty(conceptContentInstance, "scale:y", 1.0f, .3f);
 	}
 	
 	
-	private async Task TweenInfoContainerExit(Node currentInfoContainer)
+	private async Task TweenConceptContentInstanceExit(Node currentConceptContentInstance)
 	{
 		Tween tween = GetTree().CreateTween();
 		tween.SetParallel();
-		tween.TweenProperty(currentInfoContainer, "scale:x", 0.01f, .3f);
-		tween.TweenProperty(currentInfoContainer, "scale:y", 0.01f, .3f);
+		tween.TweenProperty(currentConceptContentInstance, "scale:x", 0.01f, .3f);
+		tween.TweenProperty(currentConceptContentInstance, "scale:y", 0.01f, .3f);
 		await ToSignal(tween, Tween.SignalName.Finished);
-		currentInfoContainer.QueueFree();
-		await ToSignal(currentInfoContainer, Node.SignalName.TreeExited);
+		currentConceptContentInstance.QueueFree();
+		await ToSignal(currentConceptContentInstance, Node.SignalName.TreeExited);
 	}
 }
